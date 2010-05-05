de95a7dffac0e3c263fa69c6b410dd5c680608a2
Nolan <nolan.waite@gmail.com>
2010-05-05 14:52:09 -0600

    Made WindowManPref a table data source.

--- Prefpane/Classes/WindowManPref.h
+++ Prefpane/Classes/WindowManPref.h
@@ -11,7 +11,7 @@
 
 @interface WindowManPref : NSPreferencePane 
 {
-
+  NSMutableArray *hotkeys;
 }
 
 - (void) mainViewDidLoad;
--- Prefpane/Classes/WindowManPref.m
+++ Prefpane/Classes/WindowManPref.m
@@ -11,8 +11,50 @@
 
 @implementation WindowManPref
 
+- (id)initWithBundle:(NSBundle *)bundle
+{
+  self = [super initWithBundle:bundle];
+  if (self != nil)
+  {
+    hotkeys = [[NSMutableArray alloc] initWithObjects:@"", @"", @"", nil];
+  }
+  
+  return self;
+}
+
+- (void)dealloc
+{
+  [hotkeys release];
+  
+  [super dealloc];
+}
+
 - (void) mainViewDidLoad
 {
+}
+
+// NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+  return 3;
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)column row:(NSInteger)rowIndex
+{
+  if ([[column identifier] isEqual:@"name"])
+  {
+    return [[NSArray arrayWithObjects:@"One", @"Two", @"Three", nil] objectAtIndex:rowIndex];
+  }
+  else
+  {
+    return [hotkeys objectAtIndex:rowIndex];
+  }
+}
+
+- (void)tableView:(NSTableView *)tableView setObjectValue:(id)value forTableColumn:(NSTableColumn *)column row:(NSInteger)rowIndex
+{
+  [hotkeys replaceObjectAtIndex:rowIndex withObject:value];
 }
 
 @end
