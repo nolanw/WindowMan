e51e2e9abbeab11d7bcf6c7b2f2b9335532cc239
Nolan <nolan.waite@gmail.com>
2010-05-04 22:27:38 -0600

    Added some external, relevant code.

---
+++ Global Sources/NWHotkeyBox.h
@@ -0,0 +1,43 @@
+//
+//  NWHotkeyBox.h
+//  HotkeyControl
+//
+//  Created by Nolan Waite on 10-05-03.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+// Setting the keyCode to this value represents an unset hotkey.
+extern const NSInteger NWHotkeyBoxEmpty;
+
+// Convert Carbon modifier flags to Cocoa modifier flags. Lifted from ShortcutRecorder.
+extern NSUInteger SRCarbonToCocoaFlags(NSUInteger carbonFlags);
+
+// Convert Cocoa modifier flags to Carbon modifier flags. Lifted from ShortcutRecorder.
+extern NSUInteger SRCocoaToCarbonFlags(NSUInteger cocoaFlags);
+
+// HotkeyBox is a small wrapper around a hotkey, which is a pairing of a keycode 
+// (the weird hardware-agnostic code that uniquely identifies a keyboard key) and modifier 
+// flags (i.e. function, control, option, command). It's key-value observable and 
+// Carbon/Cocoa-agnostic for the modifier flags.
+@interface NWHotkeyBox : NSObject <NSCopying>
+{
+  NSInteger keyCode;
+  NSUInteger modifierFlags;
+}
+
+@property (nonatomic, assign) NSInteger keyCode;
+@property (nonatomic, assign) NSUInteger modifierFlags;
+@property (nonatomic, assign) NSUInteger carbonModifierFlags;
+
+// Returns an autoreleased instance of HotkeyBox using designated initializer.
++ (id)hotkeyBoxWithKeyCode:(NSInteger)aKeyCode modifierFlags:(NSUInteger)someModifierFlags;
+
+// Designated initializer.
+- (id)initWithKeyCode:(NSInteger)aKeyCode modifierFlags:(NSUInteger)someModifierFlags;
+
+// Wraps designated initializer, converting modifier flags to Cocoa flags first.
+- (id)initWithKeyCode:(NSInteger)aKeyCode carbonModifierFlags:(NSUInteger)someCarbonModifierFlags;
+
+@end
---
+++ Global Sources/NWHotkeyBox.m
@@ -0,0 +1,102 @@
+//
+//  NWHotkeyBox.m
+//  HotkeyControl
+//
+//  Created by Nolan Waite on 10-05-03.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+#import "NWHotkeyBox.h"
+#import <Carbon/Carbon.h>
+
+const NSInteger NWHotkeyBoxEmpty = -1;
+
+// Following two sniped from ShortcutRecorder (BSD license).
+// TODO: ensure we're following their license here.
+
+NSUInteger SRCarbonToCocoaFlags(NSUInteger carbonFlags)
+{
+	NSUInteger cocoaFlags = 0;
+	
+	if (carbonFlags & cmdKey) cocoaFlags |= NSCommandKeyMask;
+	if (carbonFlags & optionKey) cocoaFlags |= NSAlternateKeyMask;
+	if (carbonFlags & controlKey) cocoaFlags |= NSControlKeyMask;
+	if (carbonFlags & shiftKey) cocoaFlags |= NSShiftKeyMask;
+	if (carbonFlags & NSFunctionKeyMask) cocoaFlags |= NSFunctionKeyMask;
+	
+	return cocoaFlags;
+}
+
+NSUInteger SRCocoaToCarbonFlags(NSUInteger cocoaFlags)
+{
+	NSUInteger carbonFlags = 0;
+	
+	if (cocoaFlags & NSCommandKeyMask) carbonFlags |= cmdKey;
+	if (cocoaFlags & NSAlternateKeyMask) carbonFlags |= optionKey;
+	if (cocoaFlags & NSControlKeyMask) carbonFlags |= controlKey;
+	if (cocoaFlags & NSShiftKeyMask) carbonFlags |= shiftKey;
+	if (cocoaFlags & NSFunctionKeyMask) carbonFlags |= NSFunctionKeyMask;
+	
+	return carbonFlags;
+}
+
+@implementation NWHotkeyBox
+
+@synthesize keyCode;
+@synthesize modifierFlags;
+@dynamic carbonModifierFlags;
+
+- (NSUInteger)carbonModifierFlags
+{
+  return SRCocoaToCarbonFlags(self.modifierFlags);
+}
+
+- (void)setCarbonModifierFlags:(NSUInteger)flags
+{
+  self.modifierFlags = SRCarbonToCocoaFlags(flags);
+}
+
++ (id)hotkeyBoxWithKeyCode:(NSInteger)aKeyCode modifierFlags:(NSUInteger)someModifierFlags
+{
+  return [[[self alloc] initWithKeyCode:aKeyCode modifierFlags:someModifierFlags] autorelease];
+}
+
+- (id)initWithKeyCode:(NSInteger)aKeyCode modifierFlags:(NSUInteger)someModifierFlags
+{
+  self = [super init];
+  if (self != nil)
+  {
+    keyCode = aKeyCode;
+    modifierFlags = someModifierFlags;
+  }
+  
+  return self;
+}
+
+- (id)initWithKeyCode:(NSInteger)aKeyCode carbonModifierFlags:(NSUInteger)someCarbonModifierFlags
+{
+  return [self initWithKeyCode:aKeyCode modifierFlags:SRCarbonToCocoaFlags(someCarbonModifierFlags)];
+}
+
+// NSCopying
+
+- (id)copyWithZone:(NSZone *)zone
+{
+  return [[[self class] allocWithZone:zone] initWithKeyCode:self.keyCode modifierFlags:self.modifierFlags];
+}
+
+// NSKeyValueObserving
+
++ (NSSet *)keyPathsForValuesAffectingCarbonModifierFlags
+{
+  return [NSSet setWithObject:@"modifierFlags"];
+}
+
+// NSObject
+
+- (NSString *)description
+{
+  return [[super description] stringByAppendingString:[NSString stringWithFormat:@"(keyCode: %d, modifierFlags: %d", keyCode, modifierFlags]];
+}
+
+@end
