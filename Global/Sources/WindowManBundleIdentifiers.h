78f780496f9ca29906c77ef5ceea09e38bf3a82b
Nolan <nolan.waite@gmail.com>
2010-05-06 22:42:35 -0600

    Added constants for bundle identifiers in WindowMan.

---
+++ Global/Sources/WindowManBundleIdentifiers.h
@@ -0,0 +1,10 @@
+//
+//  WindowManBundleIdentifiers.h
+//  WindowMan
+//
+//  Created by Nolan Waite on 10-05-06.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+extern const NSString * WindowManHelperBundleIdentifier;
+extern const NSString * WindowManMenuBundleIdentifier;
---
+++ Global/Sources/WindowManBundleIdentifiers.m
@@ -0,0 +1,13 @@
+//
+//  WindowManBundleIdentifiers.m
+//  WindowMan
+//
+//  Created by Nolan Waite on 10-05-06.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+#import "WindowManBundleIdentifiers.h"
+
+
+const NSString * WindowManHelperBundleIdentifier = @"ca.nolanw.WindowMan.Helper";
+const NSString * WindowManMenuBundleIdentifier = @"ca.nolanw.WindowMan.Menu";
