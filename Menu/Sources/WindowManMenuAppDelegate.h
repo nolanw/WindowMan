37eb15068934eb217be49467f32d59385a63ec5a
Nolan <nolan.waite@gmail.com>
2010-05-08 19:19:57 -0600

    Added target for Menu app.

---
+++ Menu/Sources/WindowManMenuAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  WindowManMenuAppDelegate.h
+//  WindowMan
+//
+//  Created by Nolan Waite on 10-05-08.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface WindowManMenuAppDelegate : NSObject {
+
+}
+
+@end
---
+++ Menu/Sources/WindowManMenuAppDelegate.m
@@ -0,0 +1,14 @@
+//
+//  WindowManMenuAppDelegate.m
+//  WindowMan
+//
+//  Created by Nolan Waite on 10-05-08.
+//  Copyright 2010 Nolan Waite. All rights reserved.
+//
+
+#import "WindowManMenuAppDelegate.h"
+
+
+@implementation WindowManMenuAppDelegate
+
+@end
